c3954ee5508f71d81336db38be3a8f543d85977d
yannn <yann@novalend.com>
2025-02-04 17:57:03 +0100

    Code horrible

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -10,49 +10,14 @@ use Doctrine\ORM\Mapping as ORM;
 class Invoice
 {
     #[ORM\Id, ORM\GeneratedValue, ORM\Column(type: 'integer')]
-    private int $id;
+    public int $id;
 
     #[ORM\Column(type: 'string')]
-    private string $name;
+    public string $name;
 
     #[ORM\Column(type: 'float')]
-    private float $amount;
+    public float $amount;
 
     #[ORM\Column(type: 'string')]
-    private string $currency;
-
-    public function getId(): int
-    {
-        return $this->id;
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
-
-    public function getAmount(): float
-    {
-        return $this->amount;
-    }
-
-    public function setAmount(float $amount): void
-    {
-        $this->amount = $amount;
-    }
-
-    public function getCurrency(): string
-    {
-        return $this->currency;
-    }
-
-    public function setCurrency(string $currency): void
-    {
-        $this->currency = $currency;
-    }
+    public string $currency;
 }
--- src/Service/InvoiceParser.php
+++ src/Service/InvoiceParser.php
@@ -17,24 +17,52 @@ class InvoiceParser
         $this->em = $em;
     }
 
-    public function parse(string $filePath): void
+    public function parse(string $fp): void
     {
-        if (str_contains($filePath, 'json')) {
-            $data = json_decode(file_get_contents($filePath), true);
-            foreach ($data as $row) {
-                $this->em->getConnection()->executeStatement(
-                    "UPDATE invoice SET amount = {$row['montant']} WHERE name = '{$row['nom']}'"
-                );
+        if (str_contains($fp, 'json')) {        //Pour les json
+            $f = file_get_contents($fp);
+            $d = preg_split("/\r\n|\n|\r/", $f);
+            $c = 0;
+            $m = "";
+            $n = "";
+            /** Tant qu'il y a une ligne */
+            while(true){
+                if(isset($d[$c])){
+                    if(str_contains($d[$c], "montant")){
+                    $m = explode(": ", $d[$c])[1];
+                    $m = substr($m, 0, strlen($m) - 1);
+                    }
+                    if(str_contains($d[$c], "nom")){
+                    $n = explode(": ", $d[$c])[1];
+                    $n = substr($n, 0, strlen($n) - 1);
+                    }
+                    if(str_contains($d[$c], "}")){
+                        $this->em->getConnection()->executeStatement(
+                "UPDATE invoice SET amount = {$m} WHERE name = '{$n}'"
+                    );
+                    }
+                    $c++;
+                }else{
+                break;
+                }
             }
-        } elseif (str_contains($filePath, 'csv')) {
-            $data = array_map(function($row) {
-                return str_getcsv($row, "\t");
-            }, file($filePath));
-            foreach ($data as $row) {
-                $this->em->getConnection()->executeStatement(
-                    "UPDATE invoice SET amount = {$row[0]} WHERE name = '{$row[2]}'"
-                );
-            }
+        } elseif (str_contains($fp, 'csv')) {   //Pour les json
+
+
+            $d = array_map(function($r) {
+                return str_getcsv($r, "\t");
+            }, file($fp));
+            $c = 0;
+                while(true){
+                if(isset($d[$c])){
+                    $this->em->getConnection()->executeStatement(
+                        "UPDATE invoice SET amount = {$d[$c][0]} WHERE name = '{$d[$c][2]}'"
+                    );
+                    $c++;
+                }else{
+                    break;
+                }
+                }
         }
     }
 }
